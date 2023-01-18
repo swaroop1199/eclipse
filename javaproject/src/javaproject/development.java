c33ad8adea4a08945812ea450954b2a84850eb1d
swaroop1199 <hiremathswaroop@gmail.com>
2023-01-18 15:56:27 +0530

    Sixth eclipse commit

--- javaproject/src/javaproject/development.java
+++ javaproject/src/javaproject/development.java
@@ -4,12 +4,16 @@ public class development {
 
 	public static void main(String[] args) {
 		
-		int age=25;
+		int age=15;
 		System.out.println("IT Development");
 		
 		if(age>20) 
 		{	
 		System.out.println("Ok");
+		}
+		else if (age<20)
+		{
+		System.out.println("Fair");
 		}
 		else 
 		{	
