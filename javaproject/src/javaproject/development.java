8a0b253f96772adc44f8798f92f4af1a16dfcf06
swaroop1199 <hiremathswaroop@gmail.com>
2023-01-18 16:08:41 +0530

    Seventh eclipse commit

--- javaproject/src/javaproject/development.java
+++ javaproject/src/javaproject/development.java
@@ -4,22 +4,24 @@ public class development {
 
 	public static void main(String[] args) {
 		
-		int age=15;
+		int num=9;
 		System.out.println("IT Development");
 		
-		if(age>20) 
-		{	
-		System.out.println("Ok");
+		switch (num) {
+		case 1: System.out.println("One");
+				break;
+		case 2: System.out.println("Two");
+				break;
+		case 3: System.out.println("Three");
+				break;
+		case 4: System.out.println("Four");
+				break;
+		case 5: System.out.println("Five");
+				break;
+		default: System.out.println("Default");
+				break;
 		}
-		else if (age<20)
-		{
-		System.out.println("Fair");
-		}
-		else 
-		{	
-		System.out.println("Not Ok");
-		}
-		
+
 		
 		}
 	}
