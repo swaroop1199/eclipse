f7b4e39a217db1f2e6db5083dc891b062ef4219c
swaroop1199 <hiremathswaroop@gmail.com>
2023-01-20 19:15:23 +0530

    Eleventh eclipse commit

---
+++ javaproject/src/javaproject/demo.java
@@ -0,0 +1,22 @@
+package javaproject;
+
+public class demo {
+	
+	int a;
+	int b;
+	int c;
+	
+	demo(){
+		a = 1;
+		b = 2;
+		c = 3;
+	}
+
+	
+	public static void display() {
+		System.out.println("Swaroop");
+	}
+	
+	
+	
+}
--- javaproject/src/javaproject/development.java
+++ javaproject/src/javaproject/development.java
@@ -1,15 +1,12 @@
 package javaproject;
 
-import javaproject2.development2;
-
 
 public class development {
 
 	public static void main(String[] args) {
 		
-		development2 d = new development2();
-		d.display();
 		
+		demo.display();
 		
 		
 		}
